dd6afef34070c3e3250a9fc3bfa90cebf7010f44
Salman472 <mds619820@gmail.com>
2026-07-29 22:47:35 +0600

    fix: complete product controller response handling

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -2,15 +2,22 @@ import { Request, Response } from "express";
 import catchAsync from "../../../utils/catchAsync.js";
 import { productService } from "./product.service.js";
 import sendResponse from "../../../utils/sendResponse.js";
+import httpStatus from "http-status";
 
 const createProduct=catchAsync(
     async(req:Request, res:Response)=>{
         const user=req.body
         const result=await productService.createProduct(req.body, user.id)
 
-        // sendResponse(res,{
-        //     success:true,
-        //     statusCode:httpSt
-        // })
+        sendResponse(res,{
+            success:true,
+            statusCode:httpStatus.CREATED,
+            message: "Product created successfully",
+            data: result
+        })
 }
 )
+
+export const productController = {
+  createProduct
+}
