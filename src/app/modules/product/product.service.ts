741b4b9caf7e38e62dca521705094433a4e4e9cd
Salman472 <mds619820@gmail.com>
2026-07-29 22:47:25 +0600

    feat: implement product creation service layer

---
+++ src/app/modules/product/product.service.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../../../lib/prisma.js";
+import { TProduct } from "./product.interface.js";
+
+const createProduct=async(payload:TProduct, userId:string)=>{
+const result=await prisma.product.create({
+    data:{
+        ...payload,
+        createdBy:userId
+    }
+})
+return result
+}
+
+// product controller
+export const productService={
+    createProduct
+}
