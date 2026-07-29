3aa8c2d02b5e63c6ce8cbacaf71055b28abf52c4
Salman472 <mds619820@gmail.com>
2026-07-29 22:48:02 +0600

    refactor: optimize imports in product service

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,17 +1,16 @@
 import { prisma } from "../../../lib/prisma.js";
 import { TProduct } from "./product.interface.js";
 
-const createProduct=async(payload:TProduct, userId:string)=>{
-const result=await prisma.product.create({
-    data:{
-        ...payload,
-        createdBy:userId
+const createProduct = async (payload: TProduct, userId: string) => {
+  const result = await prisma.product.create({
+    data: {
+      ...payload,
+      createdBy: userId
     }
-})
-return result
-}
+  });
+  return result;
+};
 
-// product controller
-export const productService={
-    createProduct
-}
+export const productService = {
+  createProduct
+};
