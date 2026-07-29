4dfe742d839ba8b0b11bb9d50e4c9fb676ee05a6
Salman472 <mds619820@gmail.com>
2026-07-29 22:47:02 +0600

    feat: implement auth middleware for JWT token validation

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express"
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import env from "../../config/env.js"
+
+const auth =(...requiredRoles:string[])=>{
+    return async(req:Request, res:Response, next:NextFunction)=>{
+        try {
+            const token=req.headers.authorization
+            if(!token){
+                throw new Error('you are not authorized')
+            }
+            const actualToken=token.startsWith('Bearer ') ? token.split(' ')[1]:token
+
+            const decoded=jwt.verify(
+            actualToken!, env.jwt_secret!
+            ) as JwtPayload
+
+            req.user=decoded
+
+            if(requiredRoles.length && !requiredRoles.includes(decoded.role)){
+                throw new Error('forbidden access')
+            }
+            next()
+
+        } catch (error) {
+            next(error)
+        }
+    }
+}
