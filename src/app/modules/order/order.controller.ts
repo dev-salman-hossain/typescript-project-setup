9e44ee873d435addbccc3a7584ed3f2204ae1a88
Salman472 <mds619820@gmail.com>
2026-07-30 23:20:27 +0600

    refactor: clean up unused imports in order controller

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
 import catchAsync from '../../../utils/catchAsync.js';
 import { orderService } from './order.service.js';
 import sendResponse from '../../../utils/sendResponse.js';
 import httpStatus from 'http-status';
+import { Request, Response } from 'express';
 
 const createOrder = catchAsync(async (req: Request, res: Response) => {
   const user = req.user;
