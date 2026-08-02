3edfec8334f019bac85aa6a218bcf11468970719
Salman472 <mds619820@gmail.com>
2026-08-02 23:16:05 +0600

    feat: add zod validation schemas for auth login and register

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,24 +1,29 @@
-import { z } from "zod";
-// register user
-export const registerValidationSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters"),
+﻿import { z } from "zod";
 
-  email: z.email("Invalid email address"),
-
-  password: z.string().min(6, "Password must be at least 6 characters"),
+export const loginSchema = z.object({
+  body: z.object({
+    email: z
+      .string({ required_error: "Email is required" })
+      .email("Invalid email format"),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, "Password must be at least 6 characters"),
+  }),
 });
 
-// login user
-export const loginValidationSchema = z.object({
-  email: z.email("Invalid email"),
-  password: z.string().min(6, "Password required"),
+export const registerSchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: "Name is required" }).min(2),
+    email: z
+      .string({ required_error: "Email is required" })
+      .email("Invalid email format"),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, "Password must be at least 6 characters"),
+  }),
 });
 
-export const verifyOtpValidationSchema = z.object({
-  email: z.email(),
-  otp: z.string().length(6),
-});
-
-export type RegisterInput = z.infer<typeof registerValidationSchema>;
-export type LogInInput = z.infer<typeof loginValidationSchema>;
-export type OtpInput = z.infer<typeof verifyOtpValidationSchema>;
+export const AuthValidation = {
+  loginSchema,
+  registerSchema,
+};
