39a3484c88f910d97b1822f61f588f3a89d93e80
Salman472 <mds619820@gmail.com>
2026-08-02 23:15:05 +0600

    fix: replace console.log with console.error in catch block

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,23 @@
-import { Server } from "http";
+﻿import { Server } from "http";
 import app from "./app.js";
 import env from "./config/env.js";
 
 let server: Server;
 
+// Bootstrap server startup sequence
 const bootstrap = async () => {
   try {
     server = app.listen(env.port, () => {
       console.log(`Server is running on port ${env.port}`);
     });
   } catch (err) {
-    console.log("Server running error",err);
+    console.error("Server running error", err);
   }
 };
 
 (async () => {
   await bootstrap();
-
 })();
-
-
-
 
 process.on("unhandledRejection", () => {
   console.log("Unhandled Rejection Detected");
