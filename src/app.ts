2ce7bbac86194f7ecaecf936fc417b2765eefdad
Salman472 <mds619820@gmail.com>
2026-08-02 23:15:15 +0600

    feat: add urlencoded middleware to app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,18 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+﻿import express, { Application } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import router from "./app/routes/routes.js";
 import notFound from "./app/middleware/notFound.js";
-
 import globalError from "./app/middleware/globalError.js";
 
-const app : Application = express();
+const app: Application = express();
 
-
+// Middleware setup
 app.use(cors());
-
 app.use(cookieParser());
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
 
